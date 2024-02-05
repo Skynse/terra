639dd23b0b4f0208ea384f1f4f3089c108083416
Austin A. <prodbyskynse@gmail.com>
2024-02-04 21:20:52 -0600

    redesign custom camera view and fix gallery

--- lib/pages/camera_screen.dart
+++ lib/pages/camera_screen.dart
@@ -7,6 +7,7 @@ import 'package:terra/core/ml_service.dart';
 import 'package:terra/main.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:terra/pages/gallery.dart';
 
 class CameraScreen extends StatefulWidget {
   @override
@@ -25,8 +26,8 @@ class _CameraScreenState extends State<CameraScreen> {
   @override
   void dispose() {
     // TODO: implement dispose
-    super.dispose();
     controller?.dispose();
+    super.dispose();
   }
 
   @override
@@ -51,7 +52,7 @@ class _CameraScreenState extends State<CameraScreen> {
   }
 
   Future<void> analyseImage(CameraImage cameraImage) async {
-    // if image is still analyze, skip this frame
+    // if image is still analyzing, skip this frame
     if (_isProcessing) {
       return;
     }
@@ -61,7 +62,7 @@ class _CameraScreenState extends State<CameraScreen> {
     _isProcessing = false;
 
     if (classification != null) {
-      print("CLASSIFICATION OUTPUT:" + classification.toString());
+      //print("CLASSIFICATION OUTPUT:" + classification.toString());
       if (classification!.keys.contains('positive')) {
         if (classification!['positive']! > 0.8) {
           isImageGood = true;
@@ -112,127 +113,164 @@ class _CameraScreenState extends State<CameraScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.black,
-        body: Padding(
-          padding: EdgeInsets.only(
-              bottom: 20), // add padding (space) to bottom of screen
-          child: Stack(
-            children: [
-              Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                    width: 5,
-                    color: isImageGood ? Colors.green : Colors.red,
-                  )),
-                  child: (this.controller != null &&
-                          this
-                              .controller!
-                              .value
-                              .isInitialized) // condition to make sure the camera is initialized
-                      ? CameraPreview(this.controller!)
-                      : Center(child: CircularProgressIndicator())),
-              Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      InkWell(
-                        // The button itself
-                        borderRadius: BorderRadius.circular(80),
-                        onTap: () async {
-                          // ensure camera is initialized
-                          try {
-                            await Permission.camera.onGrantedCallback(() async {
-                              await controller
-                                  ?.takePicture()
-                                  .then((path) async {
-                                // save path to gallery
-
-                                // get the temporary location of the saved file
-
-                                var file = File(path.path);
-
-                                var documentsDir =
-                                    await getExternalStorageDirectories(
-                                        type: StorageDirectory
-                                            .downloads); // get the documents folder of the current device
-
-                                var currentTime = DateTime.now()
-                                    .millisecondsSinceEpoch; // get the current date and time
-
-                                var format = file.path
-                                    .split('.')
-                                    .last; // file.png -> only get png part "png" // get the file format of the captured image
-
-                                await file.copy(
-                                    "${documentsDir?[0].path}/$currentTime.$format"); // Documents/0904203.png //copy the image from the temporary location to the permanent one
-
-                                // preview
-                                // print out image path
-                                if (!mounted) return;
-
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => Scaffold(
-                                              appBar: AppBar(
-                                                title: Text("Preview"),
-                                              ),
-                                              body: Center(
-                                                child: Image.file(
-                                                  File(
-                                                      "${documentsDir?[0].path}/$currentTime.$format"),
-                                                  fit: BoxFit.cover,
-                                                ),
-                                              ),
-                                            )));
-                              });
-                            }).request();
-                          } on CameraException catch (e) {
-                            print("Unable to initialize camera: $e");
-                          }
-                        },
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: [
-                            Icon(Icons.circle, size: 80, color: Colors.grey),
-                            Icon(Icons.circle, size: 70, color: Colors.white),
-                          ],
-                        ),
-                      ),
-                      IconButton(
-                        icon: Icon(Icons.flip_camera_android_outlined),
-                        onPressed: () async {
-                          var currentDescription = controller!.description;
-                          // ensure camera is initialized
-                          try {
-                            await Permission.camera.onGrantedCallback(() async {
-                              if (currentDescription.lensDirection ==
-                                  CameraLensDirection.front) {
-                                await controller
-                                    ?.setDescription(cameras.where((element) {
-                                  return element.lensDirection ==
-                                      CameraLensDirection.back;
-                                }).first);
-                              } else {
-                                await controller
-                                    ?.setDescription(cameras.where((element) {
-                                  return element.lensDirection ==
-                                      CameraLensDirection.front;
-                                }).first);
-                              }
-                            }).request();
-                          } on CameraException catch (e) {
-                            print("Unable to initialize camera: $e");
-                          }
-                        },
-                      ),
-                    ],
-                  )),
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Camera"),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Gallery()));
+                  },
+                  icon: Icon(Icons.browse_gallery_outlined))
             ],
           ),
-        ));
+          backgroundColor: Colors.black,
+          body: Padding(
+            padding: EdgeInsets.only(
+                bottom: 20), // add padding (space) to bottom of screen
+            child: Column(
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      Container(
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 5,
+                            ),
+                          ),
+                          child: (controller != null &&
+                                  this
+                                      .controller!
+                                      .value
+                                      .isInitialized) // condition to make sure the camera is initialized
+                              ? CameraPreview(this.controller!)
+                              : Center(child: CircularProgressIndicator())),
+                      Opacity(
+                        opacity: 0.7,
+                        child: Container(
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.height - 150,
+                          decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 5,
+                              color: (isImageGood) ? Colors.green : Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 100,
+                  child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(80),
+                        ),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            InkWell(
+                              // The button itself
+                              borderRadius: BorderRadius.circular(80),
+                              onTap: () async {
+                                // ensure camera is initialized
+
+                                try {
+                                  await Permission.camera
+                                      .onGrantedCallback(() async {
+                                    if (controller!.value.isTakingPicture) {
+                                      return;
+                                    }
+                                    await controller?.takePicture().then(
+                                      (path) async {
+                                        // save path to gallery
+
+                                        // get the temporary location of the saved file
+
+                                        var file = File(path.path);
+
+                                        var documentsDir =
+                                            await getExternalStorageDirectories(
+                                                type: StorageDirectory
+                                                    .pictures); // get the documents folder of the current device
+
+                                        var currentTime = DateTime.now()
+                                            .millisecondsSinceEpoch; // get the current date and time
+
+                                        var format = file.path
+                                            .split('.')
+                                            .last; // file.png -> only get png part "png" // get the file format of the captured image
+
+                                        await file.copy(
+                                            "${documentsDir?[0].path}/$currentTime.$format"); // Documents/0904203.png //copy the image from the temporary location to the permanent one
+                                        print(
+                                            "Image saved to ${documentsDir?[0].path}/$currentTime.$format");
+                                        // preview
+                                        // print out image path
+                                        if (!mounted) return;
+                                      },
+                                    );
+                                  }).request();
+                                } on CameraException catch (e) {
+                                  print("Unable to initialize camera: $e");
+                                }
+                              },
+                              child: const Stack(
+                                alignment: Alignment.center,
+                                children: [
+                                  Icon(Icons.circle,
+                                      size: 80, color: Colors.grey),
+                                  Icon(Icons.circle,
+                                      size: 70, color: Colors.white),
+                                ],
+                              ),
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.flip_camera_android_outlined),
+                              onPressed: () async {
+                                var currentDescription =
+                                    controller!.description;
+                                // ensure camera is initialized
+                                try {
+                                  await Permission.camera
+                                      .onGrantedCallback(() async {
+                                    if (currentDescription.lensDirection ==
+                                        CameraLensDirection.front) {
+                                      await controller?.setDescription(
+                                          cameras.where((element) {
+                                        return element.lensDirection ==
+                                            CameraLensDirection.back;
+                                      }).first);
+                                    } else {
+                                      await controller?.setDescription(
+                                          cameras.where((element) {
+                                        return element.lensDirection ==
+                                            CameraLensDirection.front;
+                                      }).first);
+                                    }
+                                  }).request();
+                                } on CameraException catch (e) {
+                                  print("Unable to initialize camera: $e");
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      )),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
 
--- lib/pages/gallery.dart
+++ lib/pages/gallery.dart
@@ -6,50 +6,73 @@ import 'package:camera/camera.dart';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'dart:typed_data';
 
-class ImageCard extends StatelessWidget {
+class ImageCard extends ConsumerWidget {
   final String imagePath;
   final Function(String) onImageSelected;
 
-  const ImageCard({required this.imagePath, required this.onImageSelected});
+  const ImageCard(
+      {Key? key, required this.imagePath, required this.onImageSelected})
+      : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          SizedBox(
-            height: 10,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () {
-                    onImageSelected(imagePath);
-                  },
-                  icon: Icon(Icons.delete),
-                ),
-              ],
+  Widget build(BuildContext context, WidgetRef ref) {
+    return InkWell(
+      onLongPress: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Delete Image"),
+                content: Text("Are you sure you want to delete this image?"),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel"),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      onImageSelected(imagePath);
+                      Navigator.pop(context);
+                    },
+                    child: Text("Delete"),
+                  ),
+                ],
+              );
+            });
+      },
+      child: SizedBox(
+        height: 300,
+        width: 300,
+        child: Column(
+          children: [
+            Expanded(
+              child: Image.file(
+                width: 200,
+                scale: 1.0,
+                File(imagePath),
+                fit: BoxFit.fitWidth,
+              ),
             ),
-          ),
-          Image.file(
-            File(imagePath),
-            fit: BoxFit.cover,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 }
 
-class Gallery extends StatefulWidget {
+class Gallery extends ConsumerStatefulWidget {
   const Gallery({super.key});
 
   @override
-  State<Gallery> createState() => _GalleryState();
+  ConsumerState<Gallery> createState() => _GalleryState();
 }
 
-class _GalleryState extends State<Gallery> {
+class _GalleryState extends ConsumerState<Gallery> {
   Future<List<String>> getImages() async {
     // get image paths
     final directory =
@@ -57,7 +80,9 @@ class _GalleryState extends State<Gallery> {
     final dir = Directory(directory![0].path);
     final files = dir.listSync();
     final imageFiles = files.where((element) {
-      return element.path.endsWith(".jpg");
+      return element.path.endsWith(".jpg") ||
+          element.path.endsWith(".jpeg") ||
+          element.path.endsWith(".png");
     }).toList();
 
     return imageFiles.map((e) => e.path).toList();
@@ -83,35 +108,61 @@ class _GalleryState extends State<Gallery> {
         title: Text("Gallery"),
       ),
       body: SafeArea(
-        child: Container(
-          child: FutureBuilder(
-            future: getImages(),
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                  itemCount: snapshot.data?.length,
-                  itemBuilder: (context, index) {
-                    isImageValid(File(snapshot.data![index]).readAsBytesSync())
-                        .then((value) {
-                      if (!value) {
-                        File(snapshot.data![index]).delete();
+        child: FutureBuilder(
+          future: getImages(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0,
+                ),
+                itemCount: snapshot.data?.length,
+                itemBuilder: (context, index) {
+                  final imagePath = snapshot.data![index];
+                  return FutureBuilder(
+                    future: File(imagePath).readAsBytes(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final rawList = snapshot.data as List<int>;
+                        return FutureBuilder(
+                          future: isImageValid(rawList),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              final isValid = snapshot.data as bool;
+                              if (isValid) {
+                                return SizedBox(
+                                  height: 200,
+                                  width: 200,
+                                  child: ImageCard(
+                                    imagePath: imagePath,
+                                    onImageSelected: (imagePath) {
+                                      File(imagePath).delete();
+                                      if (!mounted) return;
+                                      setState(() {});
+                                    },
+                                  ),
+                                );
+                              } else {
+                                return Container();
+                              }
+                            } else {
+                              return CircularProgressIndicator();
+                            }
+                          },
+                        );
+                      } else {
+                        return CircularProgressIndicator();
                       }
-                    });
-                    return ImageCard(
-                      imagePath: snapshot.data![index],
-                      onImageSelected: (imagePath) {
-                        setState(() {
-                          File(imagePath).delete();
-                        });
-                      },
-                    );
-                  },
-                );
-              } else {
-                return CircularProgressIndicator();
-              }
-            },
-          ),
+                    },
+                  );
+                },
+              );
+            } else {
+              return CircularProgressIndicator();
+            }
+          },
         ),
       ),
     );
